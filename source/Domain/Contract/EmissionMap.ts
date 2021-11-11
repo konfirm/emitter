d94d96ad1eb402df65bd3fa87e0c5b9d2304df42
Rogier Spieker <rogier@konfirm.eu>
2021-11-11 08:03:26 +0100

    Removed unused type EmissionMap

--- source/Domain/Contract/EmissionMap.ts
+++
@@ -1,5 +0,0 @@
-import { EmissionInterface } from "./EmissionInterface";
-
-export type EmissionMap<T extends { [K in EmissionInterface['type']]: EmissionInterface }> = {
-	[K in keyof T]: EmissionInterface;
-}
--- source/main.ts
+++ source/main.ts
@@ -1,5 +1,4 @@
 export type { EmissionInterface } from './Domain/Contract/EmissionInterface';
-export type { EmissionMap } from './Domain/Contract/EmissionMap';
 export type { EmissionMapper } from './Domain/Contract/EmissionMapper';
 export type { EmitterListenerInterface } from './Domain/Contract/EmitterListenerInterface';
 export { Emitter } from './Domain/Entity/Emitter';
